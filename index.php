f4492988ef5066e924e9c3040f0e9d5f912195b2
m <m>
2021-07-18 16:33:57 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+
+// define('DB_DATABASE', 'pf');    //データベース指定  
+// define('DB_USERNAME', 'pf');     //データベースユーザ
+// define('DB_PASSWORD', 'Yakie23yhe~je^');   //データベースパスワード
+// define('PDO_DSN', 'mysql:dbhost=localhost;dbname=' .DB_DATABASE);    
+// //   データベースに接続するための文字列
+
+// try {
+//     //connect
+//     $dsn = sprintf('mysql:host=%s;dbname=%s;charset=utf8;', $db['host'], $db['dbname']);
+//     $db = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD);
+//     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+//     //disconnect
+//     $db = null;
+//  }catch (PDOException $e){
+//     echo $e->getMessage();
+//     exit;
+// }
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="img/122.ico">
+    <title>pf</title>
+</head>
+<body> 
+<!--main page gronav-->
+    <header>
+    <h1><a href="index.php" alt="TOPページへ">title</a></h1>
+        <ul>
+          <li><a href=""></a></li>
+          <li><a href=""></a></li>
+          <li><a href=""></a></li>
+          <li><a href=""></a></li>
+          <li><a href="git.php">github</a></li>
+          <li><a href=""></a></li>
+        </ul>
+    </header>
+
+
+
+
+    <footer>
+        <ul>
+          <li><a href="">sample</a></li>
+          <li><a href=""></a></li>
+          <li><a href=""></a></li>
+          <li><a href=""></a></li>
+          <li><a href=""></a></li>
+          <li><a href=""></a></li>
+        </ul>
+　　</footer>
+
+<script>
+
+
+</script>
+</body>
+</html>
